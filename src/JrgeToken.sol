7f73e5790f8620f158392426cc6e68e331f1f281
aiye8497-alt <aiye8497@gmail.com>
2026-03-22 00:59:41 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    modified:   foundry.lock
    modified:   src/JrgeToken.sol

--- src/JrgeToken.sol
+++ src/JrgeToken.sol
@@ -2,12 +2,25 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/tokens/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20PAUSEABLE.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract JrgeToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
-    constructor() ERC20("JrgeToken", "JRGE") {
+    constructor() ERC20("JrgeToken", "JRGE") Ownable(msg.sender) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
+    function pause() public onlyOwner {
+        _pause();
+    }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, amount);
+    }
+}
     
